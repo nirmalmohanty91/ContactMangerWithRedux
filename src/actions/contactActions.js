fc60422df4becd29be2af48570d307c0ef99a1cd
Nirmal <nirmalmhnt@gmail.com>
2018-12-15 01:51:40 +0530

    completed redux

--- src/actions/contactActions.js
+++ src/actions/contactActions.js
@@ -1,4 +1,10 @@
-import { GET_CONTACTS, DELETE_CONTACT, ADD_CONTACT } from "./type";
+import {
+  GET_CONTACTS,
+  DELETE_CONTACT,
+  ADD_CONTACT,
+  GET_CONTACT,
+  UPDATE_CONTACT
+} from "./type";
 import axios from "axios";
 
 export const getContacts = () => async dispatch => {
@@ -10,15 +16,48 @@ export const getContacts = () => async dispatch => {
     payload: response.data
   });
 };
-export const deleteContact = id => {
-  return {
-    type: DELETE_CONTACT,
-    payload: id
-  };
+
+export const getContact = id => async dispatch => {
+  const response = await axios.get(
+    `https://jsonplaceholder.typicode.com/users/${id}`
+  );
+  dispatch({
+    type: GET_CONTACT,
+    payload: response.data
+  });
 };
-export const addContact = contact => {
-  return {
+export const deleteContact = id => async dispatch => {
+  try {
+    await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
+    dispatch({
+      type: DELETE_CONTACT,
+      payload: id
+    });
+  } catch (e) {
+    dispatch({
+      type: DELETE_CONTACT,
+      payload: id
+    });
+  }
+};
+export const addContact = contact => async dispatch => {
+  const response = await axios.post(
+    "https://jsonplaceholder.typicode.com/users",
+    contact
+  );
+  dispatch({
     type: ADD_CONTACT,
-    payload: contact
-  };
+    payload: response.data
+  });
+};
+
+export const updateContact = contact => async dispatch => {
+  const response = await axios.put(
+    `https://jsonplaceholder.typicode.com/users/${contact.id}`,
+    contact
+  );
+  dispatch({
+    type: UPDATE_CONTACT,
+    payload: response.data
+  });
 };
